bd0098be5bac472fefba850b4d746077a4f4a381
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-11-28 18:41:27 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -5,24 +5,23 @@ def network_alert_stub(celcius):
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-    if celcius <= 200:
-        return 200
-    else:
-        return 500
+    return celcius
+        
+def farenhiet_to_celcius(farenheit):
+    return (farenheit - 32) * 5 / 9
 
 def alert_in_celcius(farenheit):
-    celcius = (farenheit - 32) * 5 / 9
-    returnCode = network_alert_stub(celcius)
+    returnCode = network_alert_stub(farenhiet_to_celcius(farenheit))
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
         # However, this code doesn't count failures!
         # Add a test below to catch this bug. Alter the stub above, if needed.
+        assert returnCode == 500, f'Temperature observed is {returnCode} Celcius which is beyond Threshold Temperature 200 Celcius'
         global alert_failure_count
         alert_failure_count += 0
 
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
 print(f'{alert_failure_count} alerts failed.')
-assert(alert_failure_count == 1)
 print('All is well (maybe!)')
