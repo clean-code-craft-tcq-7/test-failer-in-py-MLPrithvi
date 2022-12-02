a167d1e7f9b222c274a620f942ad2a83feb14981
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-12-02 15:29:09 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -10,4 +10,5 @@ assert(size(37) == 'S')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
 assert(size(38) == 'S'), "Size of the T-shirt should be M"
+assert(size(42) == 'L'), "Size of the T-shirt should be L"
 print("All is well (maybe!)\n")
